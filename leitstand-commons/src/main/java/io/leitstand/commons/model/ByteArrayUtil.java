c8fff3478a79e9a1fd1e728a090446d33ad61d67
Martin Steger <47448839+martinsteger@users.noreply.github.com>
2020-03-08 18:05:28 +0100

    Introduced Resource stereotype and annotation for scope-based authentication (#6)

--- leitstand-commons/src/main/java/io/leitstand/commons/model/ByteArrayUtil.java
+++ leitstand-commons/src/main/java/io/leitstand/commons/model/ByteArrayUtil.java
@@ -18,8 +18,11 @@ package io.leitstand.commons.model;
 import static java.lang.Character.MAX_RADIX;
 import static java.util.Base64.getDecoder;
 import static java.util.Base64.getEncoder;
+import static java.util.Base64.getUrlDecoder;
+import static java.util.Base64.getUrlEncoder;
 
 import java.math.BigInteger;
+import java.util.Base64;
 
 /**
  * A bunch of byte array utilities.
@@ -61,6 +64,24 @@ public final class ByteArrayUtil {
 	 */
 	public static byte[] decodeBase64String(String base64) {
 		return getDecoder().decode(base64);
+	}
+	
+	/**
+	 * Converts a byte array to a <a href="https://en.wikipedia.org/wiki/Base64">Base64</a> URL encoded string.
+	 * @param data - the bytes to be encoded 
+	 * @return the Base64 encoded string
+	 */
+	public static String encodeBase64UrlString(byte[] bytes) {
+		return getUrlEncoder().encodeToString(bytes);
+	}
+
+	/**
+	 * Decodes a <a href="https://en.wikipedia.org/wiki/Base64">Base64</a> URL encoded string to a byte array.
+	 * @param data - the Base64 string
+	 * @return the decoded byte array
+	 */
+	public static byte[] decodeBase64UrlString(String base64) {
+		return getUrlDecoder().decode(base64);
 	}
 
 	private ByteArrayUtil() {
