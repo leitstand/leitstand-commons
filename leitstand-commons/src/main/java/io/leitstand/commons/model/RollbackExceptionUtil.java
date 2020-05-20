32dfb9d88a9b8807eebc140fba2fba614ce753ac
Martin Steger <martin@rtbrick.com>
2020-05-20 10:43:55 +0200

    Add RollbackException handling utilities

---
+++ leitstand-commons/src/main/java/io/leitstand/commons/model/RollbackExceptionUtil.java
@@ -0,0 +1,65 @@
+package io.leitstand.commons.model;
+
+import static java.lang.String.format;
+
+import java.util.function.Supplier;
+import java.util.logging.Logger;
+
+import javax.transaction.RollbackException;
+
+import io.leitstand.commons.EntityNotFoundException;
+import io.leitstand.commons.UniqueKeyConstraintViolationException;
+
+
+public class RollbackExceptionUtil {
+	
+	private static final Logger LOG = Logger.getLogger(RollbackExceptionUtil.class.getName());
+
+
+	
+	public static RollbackExceptionUtil givenRollbackException(Exception e) {
+		return new RollbackExceptionUtil(e);
+	}
+	
+	private Exception cause;
+	private Supplier<?> supplier;
+
+	
+	private RollbackExceptionUtil(Exception cause) {
+		this.cause = cause;
+	}
+
+	/**
+	 * Creates an instance to test whether an entity exists.
+	 * @param t the test to check whether an entity exists. 
+	 * @return a reference to this util to proceed with the error handling fluently.
+	 */
+	public RollbackExceptionUtil whenEntityExists(Supplier<?> supplier) {
+		this.supplier = supplier;
+		return this;
+	}
+	
+
+
+	
+	/**
+	 * Fires the specified {@link UniqueKeyConstraintViolationException} if the entity exists.
+	 * @param e the unique key constraint violation to be reported
+	 */
+	public void thenThrow(UniqueKeyConstraintViolationException e) {
+		try {
+			if(cause.getClass() != RollbackException.class) {
+				return;
+			}
+			
+			if(supplier.get() != null) {
+				throw e;
+			}
+		} catch (EntityNotFoundException notfound) {
+			// No action required.
+			LOG.fine(()->format("Entity not found: %s",e.getMessage()));
+		}
+	}
+	
+	
+}
