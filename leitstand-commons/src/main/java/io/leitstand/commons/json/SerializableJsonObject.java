f2304a0cb6c963734b23148ff0584d14d03af6cf
Martin Steger <martin@rtbrick.com>
2022-05-11 01:55:40 +0200

    Organize imports

--- leitstand-commons/src/main/java/io/leitstand/commons/json/SerializableJsonObject.java
+++ leitstand-commons/src/main/java/io/leitstand/commons/json/SerializableJsonObject.java
@@ -24,7 +24,6 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
